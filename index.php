427c4af324e47e230a8523d09e447cb9c83e3534
valho44 <89075469+valho44@users.noreply.github.com>
2021-09-10 06:19:38 +1000

    Update index.php

--- index.php
+++ index.php
@@ -5,8 +5,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <img title="Brainguard" src="Logo with name.png"/>
     <title>Brainguard</title>
-    <link rel="shortcut icon" href="/images/icon_logo.png"/>
 
     <!-- icon -->
     <link rel="icon" href="/images/icon_logo.png">
