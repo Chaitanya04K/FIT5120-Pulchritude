da0af82ffef4f896ea2b2d932e74b52067fcee21
valho44 <89075469+valho44@users.noreply.github.com>
2021-09-10 02:27:39 +1000

    Updated logo icon

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
     <title>Brainguard</title>
 
     <!-- icon -->
-    <link rel="icon" href="/images/Logo without name white.png">
+    <link rel="icon" href="/images/Logo with name2.png">
 
     <!-- style CSS -->
     <link rel="stylesheet" href="style.css">
