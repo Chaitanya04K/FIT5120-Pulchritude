766ea219e2845c84901629fe64e97e5a8e0c8325
Chaitanya04K <ckan0011@student.monash.edu>
2021-09-12 23:17:15 +1000

    fixing logout.php

--- logout.php
+++ logout.php
@@ -4,6 +4,9 @@ session_start();
 
 //Destroying the session
 if (session_destroy()) {
+
+    $_SESSION["loggedin"] = false;
+
     //Redirecting to login page
     header("location: login.php");
     exit;
