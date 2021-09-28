c2b8646f89760c2b77766ace2dc8787e92e53a32
Bozhi970513 <70011833+Bozhi970513@users.noreply.github.com>
2021-09-28 19:08:17 +0800

    c

--- getInfo2.php
+++ getInfo2.php
@@ -133,18 +133,18 @@
     $("#level").text(level);
 
 
-    $(function(){
-        function getQueryVariable(variable) {
-            var query = window.location.search.substring(1);
-            var vars = query.split("&");
-            for (var i = 0; i < vars.length; i++) {
-                var pair = vars[i].split("=");
-                if (pair[0] == variable) {
-                    return pair[1];
-                }
+    function getQueryVariable(variable) {
+        var query = window.location.search.substring(1);
+        var vars = query.split("&");
+        for (var i = 0; i < vars.length; i++) {
+            var pair = vars[i].split("=");
+            if (pair[0] == variable) {
+                return pair[1];
             }
-            return (false);
-        } 
+        }
+        return (false);
+    } 
+    $(function(){
         $("#tr1, #tr2, #tr3, #tr4").each(function() {
             var query = window.location.search.substring(1);
             var vars = query.split("&");
