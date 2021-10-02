89b2992318ab4694431b1dbc28dfe3c4065e681b
Chaitanya04K <ckan0011@student.monash.edu>
2021-10-02 11:41:01 +1000

    Working on iFrames

--- causes.php
+++ causes.php
@@ -65,13 +65,14 @@
         <p class="info-mainHeading">Videos on Depression</p>
         <div class="info-videos">
             <div>
-                <iframe width="520" height="315" src="https://www.youtube.com/embed/XiCrniLQGYc"
+                <!-- width 520 height 315 -->
+                <iframe src="https://www.youtube.com/embed/XiCrniLQGYc"
                     title="YouTube video player" frameborder="0"
                     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                     allowfullscreen></iframe>
             </div>
             <div>
-                <iframe width="500" height="315" src="https://www.youtube.com/embed/X-fAEMgQnt8"
+                <iframe src="https://www.youtube.com/embed/X-fAEMgQnt8"
                     title="YouTube video player" frameborder="0"
                     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                     allowfullscreen></iframe>
