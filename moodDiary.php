da28db649dcd1b4595894eabba6734ae50d7fe5a
Chaitanya04K <ckan0011@student.monash.edu>
2021-10-04 23:11:41 +1100

    added styling in moodDiary.php and added icon into navbar.php

--- moodDiary.php
+++ moodDiary.php
@@ -17,7 +17,11 @@ if (!isset($_SESSION["userid"]) || $_SESSION["loggedin"] !== true) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Brainguard</title>
+
+    <!-- style CSS -->
+    <link rel="stylesheet" href="style.css">
+
 </head>
 
 <body>
