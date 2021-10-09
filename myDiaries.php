82ecd24dc06855e1febab76974ab4ee4d76b865a
Chaitanya04K <ckan0011@student.monash.edu>
2021-10-09 22:15:19 +1100

    +

--- myDiaries.php
+++ myDiaries.php
@@ -132,7 +132,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['submit'])) {
     <script>
         function deleteDiary(diaryID) {
             var loggedInUser = "<?php echo $_SESSION["email"]; ?>";
-            var date = document.getElementsByClassName("title " + diaryID);
+            var date = document.getElementByClassName("title " + diaryID).innerHTML;
 
 
             console.log(loggedInUser);
