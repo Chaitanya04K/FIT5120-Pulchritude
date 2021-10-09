a80a0604be3b93375499b51eace72485ee16f2d7
Chaitanya04K <ckan0011@student.monash.edu>
2021-10-09 19:04:27 +1100

    +

--- myDiaries.php
+++ myDiaries.php
@@ -69,9 +69,9 @@ if ($query = $db->prepare("SELECT diaryDate, diaryTime, title, content FROM diar
             </div>
         </div>
         <div class="container diaryContainer">
-            <button class="btn btn-primary" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
+            <div class="btn btn-primary" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
                 Diary 1
-            </button>
+            </div>
             <div class="collapse" id="collapseExample">
                 <div class="card card-body">
                     Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident.
