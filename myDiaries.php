e51758941956cb4468016d029f7e8705e8877bc7
Chaitanya04K <ckan0011@student.monash.edu>
2021-10-09 21:38:27 +1100

    adding jquery uncompressed cdn

--- myDiaries.php
+++ myDiaries.php
@@ -125,6 +125,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['submit'])) {
 
     <?php include 'footer.php'; ?>
 
+    <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
+
     <script>
         function deleteDiary() {
             $.ajax({
