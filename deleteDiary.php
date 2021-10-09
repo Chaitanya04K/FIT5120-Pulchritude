4e3a5cd666c912e8749f26c1bcc8bb6e30b9b9c9
Chaitanya04K <ckan0011@student.monash.edu>
2021-10-09 22:53:51 +1100

    +

--- deleteDiary.php
+++ deleteDiary.php
@@ -6,8 +6,13 @@
     $title = $_POST['title'];
     $user = $_POST['user'];
     
-    if ($query = $db->prepare("DELETE FROM diary WHERE email = ? AND diaryDate = ? AND diaryTime = ? AND title = ?")) {
-        $query->bind_param('ssss', $user, $date, $time, $title);
-        $query->execute();
+    try {
+        if ($query = $db->prepare("DELETE FROM diary WHERE email = ? AND diaryDate = ? AND diaryTime = ? AND title = ?")) {
+            $query->bind_param('ssss', $user, $date, $time, $title);
+            $query->execute();
+            echo "Diary Successfully Deleted!";
+        }
+    } catch (PDOException $e) {
+        echo "Something went wrong!";
     }
 ?>
