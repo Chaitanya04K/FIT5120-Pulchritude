1daf3009410ca9a173eed7a781b68084db7f34e7
Chaitanya04K <ckan0011@student.monash.edu>
2021-10-09 22:33:58 +1100

    +

--- deleteDiary.php
+++ deleteDiary.php
@@ -4,6 +4,10 @@
     $time = $_GET['time'];
     $title = $_GET['title'];
     $user = $_GET['user'];
+    echo $date;
+    echo $time;
+    echo $title;
+    echo $user;
     function php_func($diaryDate,$diaryTime,$loggedInUser,$diaryTitle){
         echo $diaryDate . " " . $diaryTime . " " . $diaryTitle . " " . $loggedInUser;
     }
