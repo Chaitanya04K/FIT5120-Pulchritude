a2723de240796e97f221b6024745ea47ee137cff
valho44 <89075469+valho44@users.noreply.github.com>
2021-10-11 20:29:14 +1100

    Changes

--- myDiaries.php
+++ myDiaries.php
@@ -59,7 +59,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['submit'])) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>brainguard</title>
+    <title>Brainguard</title>
 
     <!-- icon -->
     <link rel="icon" href="/images/icon_logo.png">
