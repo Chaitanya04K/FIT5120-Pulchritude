22f19fcf3ed08ace904c9d4ca5a5a45628fe18ce
Chaitanya04K <ckan0011@student.monash.edu>
2021-10-14 15:43:29 +1100

    +

--- myDiaries.php
+++ myDiaries.php
@@ -163,7 +163,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['submit'])) {
 
             for (i = 0; i < x.length; i++) {
                 if (!x[i].innerHTML.toLowerCase().includes(input)) {
-                    y[i].style.display = "none !important";
+                    y[i].classList.add = "removeCard";
                 } else {
                     y[i].style.display = "list-item";
                 }
